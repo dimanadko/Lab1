4fa9bba248eb7c77856b853184712de053c50aef
dimanadko <dimanadobko@gmail.com>
2018-02-07 14:42:32 +0200

    Implent one word crypting

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -7,9 +7,30 @@
 //
 
 #include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(){
+    string message, key="key";
+    char matrix [26][26];
+    cout << "Give message: ";
+    cin >> message;
+    char crypted[message.length()];
+    for (int i=0; i<26; i++){
+        for (int j=0; j<26; j++){
+            matrix[i][j]='a'+(i+j>25?i+j-26:i+j);
+        }
+    };
+    int j = 0;
+    for (int i=0; i<message.length(); i++){
+        if(j>=key.length()){
+            j=0;
+        }
+        crypted[i]=matrix[(int)message[i]-96][(int)key[j]-96];
+        j++;
+    }
+    printf(crypted);
+    cout<<endl;
     return 0;
 }
