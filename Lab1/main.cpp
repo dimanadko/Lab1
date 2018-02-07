b5a5ae9e4094869e2a0af29f03900180d6a7f76b
dimanadko <dimanadobko@gmail.com>
2018-02-07 10:38:55 +0200

    Initial Commit

---
+++ Lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Dima on 07.02.18.
+//  Copyright © 2018 Dima. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
